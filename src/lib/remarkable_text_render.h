080664bde64768f8d9d9014358e158b11b437e43
Visti Andresen <talpa@talpa.dk>
2019-12-18 16:21:45 +0100

    Added some extra doxygen comments

--- src/lib/remarkable_text_render.h
+++ src/lib/remarkable_text_render.h
@@ -16,8 +16,32 @@ typedef struct {
   int16_t yMax;
 } TextBoundingBoundingBox;
 
+/** 
+ * Renders a text string to the frame buffer
+ * 
+ * @param screen the frame buffer to render to
+ * @param font the face / font to use
+ * @param text utf-8 string containing the text to render
+ * @param x x coordinate of the text base line start
+ * @param y y coordinate of the text base line start
+ * @param doDraw if false the function dos not render any pixels (can be used to obtain the bounding box before rendering)
+ * @param boundingBox if non NULL the bounding box information will be returned here 
+ */
 void remarkable_text_render_simple(remarkable_framebuffer *screen, FT_Face font, const char *text, int16_t x, int16_t y, uint8_t doDraw, TextBoundingBoundingBox *boundingBox);
 
+/** 
+ * Renders a text string to the frame buffer
+ * 
+ * @param screen the frame buffer to render to
+ * @param font the face / font to use
+ * @param text utf-8 string containing the text to render
+ * @param x x coordinate of the text base line start
+ * @param y y coordinate of the text base line start
+ * @param doDraw if false the function dos not render any pixels (can be used to obtain the bounding box before rendering)
+ * @param boundingBox if non NULL the bounding box information will be returned here 
+ * @param alphaIndex the index 0-15 of the colour that should be transparent (-1 for no colour), 0 for normal transparent rendering as the true type library uses 0 for "paper" and 255 for 100% ink
+ * @param lut the colour look up table that determines font colour, use lut16Inverse_ for black on white rending
+ */
 void remarkable_text_render_simple_alpha_colour(remarkable_framebuffer *screen, FT_Face font, const char *text, int16_t x, int16_t y, uint8_t doDraw, TextBoundingBoundingBox *boundingBox, int16_t alphaIndex, uint16_t lut[16]);
 
 
